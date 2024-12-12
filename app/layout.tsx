73c4e6af1a9efc66fee010250335470524e2d7e9
Tamir Alaluf <alaluf101@gmail.com>
2024-12-12 13:07:12 +0200

    fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import { Analytics } from "@vercel/analytics/react";
+import { Exo_2 } from "next/font/google";
+import { Bricolage_Grotesque } from "next/font/google";
 
 // const geistSans = localFont({
 //   src: "./fonts/GeistVF.woff",
@@ -17,13 +19,21 @@ import { Analytics } from "@vercel/analytics/react";
 //   variable: "--font-geist-mono",
 //   weight: "100 900",
 // });
-
+const bricolageGrotesque = Bricolage_Grotesque({
+  subsets: ["latin"],
+  weight: ["400", "700"], // You can specify multiple weights
+  variable: "--font-bricolage", // Optional: for using with Tailwind or CSS variables
+});
 const roboto = Roboto({
   weight: ["300", "400", "500", "700"],
   subsets: ["latin"],
   display: "swap",
 });
-
+const exo2 = Exo_2({
+  subsets: ["latin"],
+  weight: ["400", "700"], // You can specify multiple weights
+  variable: "--font-exo2", // Optional: for using with Tailwind or CSS variables
+});
 export const metadata: Metadata = {
   title: "ResuMate",
   description: "A resume builder for developers",
@@ -38,7 +48,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={roboto.className}>
+    <html lang="en" className={bricolageGrotesque.className}>
       <body>
         <ClerkProvider dynamic>
           <NavBar />
